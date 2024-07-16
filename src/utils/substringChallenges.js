5246ebaafcf2b03d81c461e404260cb1f7b4333d
Pooja G P <39986345+poojaprakashgp@users.noreply.github.com>
2024-07-16 22:58:47 +0530

    Update substringChallenges.js

--- src/utils/substringChallenges.js
+++ src/utils/substringChallenges.js
@@ -23,24 +23,37 @@ function longestsubstr(s){
 console.log(longestsubstr("acbbcdjas")) //logestsubstring with two distinct charector
 
 
-function longestsubstrWithoutRepeatingChar(s){
- let map = Array(128).fill(0);
- let count=0, end=0, begin=0, d=0, head=0;
- while(end<s.length){
-   if(map[s.charCodeAt(end++)]++>0)count++;
-   while(count>0){
-      if(map[s.charCodeAt(begin++)]-->1){
+function longestsubstrWithoutRepeatingChar(s) {
+  let map = new Map();
+  let count = 0, end = 0, begin = 0, d = 0, head = 0;
 
-      count--}
-   }
-   
-    if (end - begin > d) {
-            d = end - begin;
-            head = begin;
+  while (end < s.length) {
+    if (map.has(s[end]) && map.get(s[end]) > 0) {
+      count++;
+    }
+    map.set(s[end], (map.get(s[end]) || 0) + 1);
+    end++;
+
+    while (count > 0) {
+      if (map.has(s[begin]) && map.get(s[begin]) > 1) {
+        map.set(s[begin], map.get(s[begin]) - 1);
+        if (map.get(s[begin]) === 1) {
+          count--;
         }
- }
- console.log(head, d, s.substring(head, head+d))
- return d
+      } else {
+        map.set(s[begin], map.get(s[begin]) - 1);
+      }
+      begin++;
+    }
+
+    if (end - begin > d) {
+      d = end - begin;
+      head = begin;
+    }
+  }
+
+  console.log(head, d, s.substring(head, head + d));
+  return d;
 }
 
-console.log(longestsubstrWithoutRepeatingChar("acbbcdjas"))
+console.log(longestsubstrWithoutRepeatingChar("acbbcdjas"));
