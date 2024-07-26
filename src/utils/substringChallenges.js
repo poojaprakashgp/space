b3327390e758b89affdcd1108194fe74ceeec929
Pooja G P <39986345+poojaprakashgp@users.noreply.github.com>
2024-07-26 20:15:39 +0530

    Update substringChallenges.js

--- src/utils/substringChallenges.js
+++ src/utils/substringChallenges.js
@@ -20,6 +20,46 @@ function longestsubstr(s){
 
 
 
+function longestsubstr(s) {
+  let map = new Map();
+  let count = 0, end = 0, begin = 0, d = 0, head = 0;
+
+  while (end < s.length) {
+    if (!map.has(s[end])) {
+      map.set(s[end], 0);
+    }
+    if (map.get(s[end]) === 0) {
+      count++;
+    }
+    map.set(s[end], map.get(s[end]) + 1);
+    end++;
+
+    while (count > 2) {
+      if (map.has(s[begin])) {
+        if (map.get(s[begin]) === 1) {
+          count--;
+        }
+        map.set(s[begin], map.get(s[begin]) - 1);
+      }
+      begin++;
+    }
+
+    if (end - begin > d) {
+      d = end - begin;
+      head = begin;
+    }
+  }
+
+  console.log(head, d, s.substring(head, head + d));
+  return d;
+}
+
+// Sample Input
+console.log(longestsubstr("abcabcbb")); // Output: "bcb" or "bca" or any other valid substring
+
+  
+
+
 console.log(longestsubstr("acbbcdjas")) //logestsubstring with two distinct charector
 
 
