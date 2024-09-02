005d09b24f9f6ab2e20f2afce4421bcfed3705dc
Pooja G P <39986345+poojaprakashgp@users.noreply.github.com>
2024-09-02 16:14:53 +0530

    Update snippetBasedQa.js

--- src/utils/snippetBasedQa.js
+++ src/utils/snippetBasedQa.js
@@ -19,3 +19,14 @@ console.log(1)
 setTimeout(()=>(function(){console.log(2)})())
 console.log(3)
 })()
+
+function cun(){
+const a=b=c=1;
+/* above line can be written as below 
+c=1;
+b=c;
+const a=b;
+*/
+}
+cun();
+console.log(typeof a, typeof b, typeof c)
