4afc02b747e63dc72e17f743e9a6ce8a85d4a373
Pooja G P <39986345+poojaprakashgp@users.noreply.github.com>
2024-12-04 12:09:57 +0530

    Update rotateArrayByKTimes.js

--- src/utils/rotateArrayByKTimes.js
+++ src/utils/rotateArrayByKTimes.js
@@ -1,13 +1,15 @@
 const arr = [1, 2, 3, 4, 5];
-const k = 6;
+const k = 2;
 function rotateArryByKTimes(arr, k){
-  if(arr.length==k || k===0 || arr.length<=1 || k<0 ||k>arr.length)return arr;
-  const res = [];
-  for(let i=arr.length-k;i<arr.length;i++){
+  if(arr.length==k || k===0 || arr.length<=1 || k<0)return arr;
+  const res = [];let len;
+  len = k%arr.length;
+  console.log(len)
+  for(let i=len;i<arr.length;i++){
    res.push(arr[i])
   }
 
-  for(let i=0;i<arr.length-k;i++){
+  for(let i=0;i<len;i++){
    res.push(arr[i]);
   }
 	return res;
