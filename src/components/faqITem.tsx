fd58ff4a3579180b9953649e90f7105546473409
Pooja G P <39986345+poojaprakashgp@users.noreply.github.com>
2025-07-09 10:36:04 +0530

    Update faqITem.tsx

--- src/components/faqITem.tsx
+++ src/components/faqITem.tsx
@@ -193,3 +193,56 @@ describe('DeviceDetails', () => {
   });
 });
 
+
+  ● Test suite failed to run
+                                                                                                                                                             
+    Jest encountered an unexpected token
+
+    Jest failed to parse a file. This happens e.g. when your code or its dependencies use non-standard JavaScript syntax, or when Jest is not configured to support such syntax.
+
+    Out of the box Jest supports Babel, which will be used to transform your files into valid JS based on your Babel configuration.
+
+    By default "node_modules" folder is ignored by transformers.
+
+    Here's what you can do:
+     • If you are trying to use ECMAScript Modules, see https://jestjs.io/docs/ecmascript-modules for how to enable it.
+     • If you are trying to use TypeScript, see https://jestjs.io/docs/getting-started#using-typescript
+     • To have some of your "node_modules" files transformed, you can specify a custom "transformIgnorePatterns" in your config.
+     • If you need a custom transformation specify a "transform" option in your config.
+     • If you simply want to mock your non-JS modules (e.g. binary assets) you can stub them out with the "moduleNameMapper" config option.
+
+    You'll find more details and examples of these config options in the docs:
+    https://jestjs.io/docs/configuration
+    For information about custom transformations, see:
+    https://jestjs.io/docs/code-transformation
+
+    Details:
+
+    C:\Users\pplepo\Desktop\VProject\onevz-value-digital-mfe-shop\node_modules\@vds\core\icons\warning\index.js:7
+    import IconBase from '../icon-base';
+    ^^^^^^
+
+    SyntaxError: Cannot use import statement outside a module
+
+      12 |   className?: string;
+      13 |   closeModal: () => void;
+    > 14 |   show: boolean;
+         |                                     ^
+      15 |   showClose?: boolean;
+      16 |   modalTitle?: string | React.ReactNode;
+      17 |   titleClassName?: string;
+
+      at Runtime.createScriptFromCode (node_modules/jest-runtime/build/index.js:1505:14)
+      at Object.<anonymous> (common/molecules/Modal/Modal.tsx:14:57)
+      at Object.<anonymous> (common/templates/Recommendation/PaymentOptionsPrice.tsx:16:16)
+      at Object.<anonymous> (common/templates/Recommendation/PaymentOptions.tsx:13:69)
+      at Object.<anonymous> (common/organisms/ProductDescription/Components/DeviceDetails/index.tsx:19:64)
+      at Object.<anonymous> (common/organisms/ProductDescription/Components/DeviceDetails/test/DeviceDetails.test.tsx:42:16)
+
+Test Suites: 1 failed, 1 total                                                                                                                               
+Tests:       0 total                                                                                                                                         
+Snapshots:   0 total
+Time:        8.409 s
+Ran all test suites matching /c:\\Users\\pplepo\\Desktop\\VProject\\onevz-value-digital-mfe-shop\\common\\organisms\\ProductDescription\\Components\\DeviceDetails\\test\\DeviceDetails.test.tsx/i with tests matching "DeviceDetails".
+(node:290416) [DEP0040] DeprecationWarning: The `punycode` module is deprecated. Please use a userland alternative instead.
+(Use `node --trace-deprecation ...` to show where the warning was created)
