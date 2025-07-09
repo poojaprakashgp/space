1d2140b27bf8d41dfb5f6aba1a894783b24bdb0c
Pooja G P <39986345+poojaprakashgp@users.noreply.github.com>
2025-07-09 16:49:30 +0530

    Update new.tsx

--- src/components/new.tsx
+++ src/components/new.tsx
@@ -1,6 +1,17 @@
-  <div
+   <button
             id={id}
-            onClick={() => selectedDeviceAvailable === 'Available' && handleRadioChange(id)}
+            onClick={() =>
+              selectedDeviceAvailable === 'Available' && handleRadioChange(id)
+            }
+            onKeyDown={(e) => {
+              if (
+                selectedDeviceAvailable === 'Available' &&
+                (e.key === 'Enter' || e.key === ' ')
+              ) {
+                e.preventDefault(); 
+                handleRadioChange(id);
+              }
+            }}
             data-testid={id}
             className='payment-options__main-container'
           >
@@ -9,7 +20,7 @@
                 <RadioButton
                   disabled={selectedDeviceAvailable !== 'Available'}
                   checked={selectedRadioButton === id}
-                  aria-label={''}
+                  aria-label=''
                   id={id}
                   name='payment_option'
                   value=''
@@ -29,55 +40,68 @@
 
               {PaymentOptionsPriceDetails(id, price, SKUs, selectedSKUId)}
             </div>
-          </div>
-Visible, non-interactive elements with click handlers must have at least one keyboard listener.sonarqube(typescript:S1082)
-(property) JSX.IntrinsicElements.div: React.DetailedHTMLProps<React.HTMLAttributes<HTMLDivElement>, HTMLDivElement>
+          </button>
+'use client';
 
+import React from 'react';
 
+export interface ButtonProps {
+  primary?: boolean;
+  backgroundColor?: string;
+  size?: 'small' | 'medium' | 'large' | 'xl';
+  label: string | React.ReactNode;
+  className?: string;
+  onClick?: () => void;
+  onKeyDown?: (e: React.KeyboardEvent<HTMLButtonElement>) => void;
+  textColor?: boolean;
+  textColorValue?: string;
+  icon?: React.ReactNode;
+  disabled?: boolean;
+  dataGtmCta?: string;
+  dataTestId?: string;
+  ariaLabel?: string;
+}
 
-
-
-  <div
-  id={id}
-  role="button"
-  tabIndex={0}
-  onClick={() =>
-    selectedDeviceAvailable === 'Available' && handleRadioChange(id)
+export const Button = ({
+  primary,
+  size = 'medium',
+  label,
+  className,
+  textColor,
+  onClick,
+  textColorValue,
+  icon,
+  disabled = false,
+  dataGtmCta,
+  dataTestId,
+  ariaLabel,
+}: ButtonProps) => {
+  let textColorClass = 'button--text-white';
+  if (textColorValue) {
+    textColorClass = 'button-custom-text';
+  } else if (textColor) {
+    textColorClass = 'button--text-dark';
   }
-  onKeyDown={(e) => {
-    if (
-      selectedDeviceAvailable === 'Available' &&
-      (e.key === 'Enter' || e.key === ' ')
-    ) {
-      e.preventDefault(); // avoid scrolling on space
-      handleRadioChange(id);
-    }
-  }}
-  data-testid={id}
-  className='payment-options__main-container'
->
-  <div className='payment-options__box-container'>
-    <div className='payment-options__checkbox'>
-      <RadioButton
-        disabled={selectedDeviceAvailable !== 'Available'}
-        checked={selectedRadioButton === id}
-        aria-label=''
-        id={id}
-        name='payment_option'
-        value=''
-        label=''
-        onChange={() => {}}
-        className='payment__cardBtn'
-      />
-    </div>
+  const buttonClasses = [
+    'button',
+    primary ? 'button--primary' : 'button--secondary',
+    `button--${size}`,
+    textColorClass,
+    className,
+  ].join(' ');
+  return (
+    <button
+      type='button'
+      className={`${buttonClasses}`}
+      onClick={onClick}
+      disabled={disabled}
+      data-gtm-cta={dataGtmCta}
+      data-testid={dataTestId}
+      aria-label={ariaLabel}
+    >
+      {icon} {label}
+    </button>
+  );
+};
 
-    <div className='payment-options__body-container'>
-      <div className='payment-options__body-title'>{title}</div>
-      {body && <div className='payment-options__body-content'>{body}</div>}
-      {renderImage()}
-    </div>
-
-    {PaymentOptionsPriceDetails(id, price, SKUs, selectedSKUId)}
-  </div>
-</div>
-Use <input type="button">, <input type="image">, <input type="reset">, <input type="submit">, or <button> instead of the "button" role to ensure accessibility across all devices.s
+export default Button;
