a0dc3e33c4fadd845929f30d65d17723bbecee73
Pooja G P <39986345+poojaprakashgp@users.noreply.github.com>
2025-07-09 16:14:44 +0530

    Update new.tsx

--- src/components/new.tsx
+++ src/components/new.tsx
@@ -32,3 +32,52 @@
           </div>
 Visible, non-interactive elements with click handlers must have at least one keyboard listener.sonarqube(typescript:S1082)
 (property) JSX.IntrinsicElements.div: React.DetailedHTMLProps<React.HTMLAttributes<HTMLDivElement>, HTMLDivElement>
+
+
+
+
+
+  <div
+  id={id}
+  role="button"
+  tabIndex={0}
+  onClick={() =>
+    selectedDeviceAvailable === 'Available' && handleRadioChange(id)
+  }
+  onKeyDown={(e) => {
+    if (
+      selectedDeviceAvailable === 'Available' &&
+      (e.key === 'Enter' || e.key === ' ')
+    ) {
+      e.preventDefault(); // avoid scrolling on space
+      handleRadioChange(id);
+    }
+  }}
+  data-testid={id}
+  className='payment-options__main-container'
+>
+  <div className='payment-options__box-container'>
+    <div className='payment-options__checkbox'>
+      <RadioButton
+        disabled={selectedDeviceAvailable !== 'Available'}
+        checked={selectedRadioButton === id}
+        aria-label=''
+        id={id}
+        name='payment_option'
+        value=''
+        label=''
+        onChange={() => {}}
+        className='payment__cardBtn'
+      />
+    </div>
+
+    <div className='payment-options__body-container'>
+      <div className='payment-options__body-title'>{title}</div>
+      {body && <div className='payment-options__body-content'>{body}</div>}
+      {renderImage()}
+    </div>
+
+    {PaymentOptionsPriceDetails(id, price, SKUs, selectedSKUId)}
+  </div>
+</div>
+
