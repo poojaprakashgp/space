8b8fd571a7f745ac53684460ad65148575d10ca6
Pooja G P <39986345+poojaprakashgp@users.noreply.github.com>
2025-08-19 19:06:58 +0530

    Update new.tsx

--- src/components/new.tsx
+++ src/components/new.tsx
@@ -1,2 +1,9 @@
 https://verizon.webex.com/meet/raja.patibandla
 https://verizon.webex.com/meet/raja.patibandla
+
+
+
+
+Users don’t have to Tab through every radio option (which could be many).
+They only Tab once into the group, then use arrows to pick.
+This is the standard accessibility pattern across browsers, NVDA, JAWS, VoiceOver, TalkBack, etc.
